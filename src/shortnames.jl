32f27e8eda5c6d647b6b605012b223a14420eda8
goropikari <goropikari56@gmail.com>
2018-04-18 21:25:28 +0900

    add single unitary

--- src/shortnames.jl
+++ src/shortnames.jl
@@ -1,4 +1,4 @@
-const id() = identityoperator(SpinBasis(1//2))
+id() = identityoperator(SpinBasis(1//2))
 const X = sigmax
 const Y = sigmay
 const Z = sigmaz
@@ -8,9 +8,16 @@ const Sdg = sdggate
 const T = tgate
 const Tdg = tdggate
 const U1 = phaseshift
+"""
+    U2(ϕ::Real, λ::Real)
+
+Equivalent to `sigleunitary(π/2, ϕ, λ)`.
+"""
+U2(ϕ::Real, λ::Real) = sigleunitary(π/2, ϕ, λ)
+const U3 = sigleunitary
 const CU = controlsgate
 const CNOT = cnot
 const CCX = toffoli
 const SWAP = swap
 
-export id, X, Y, Z, H, S, Sdg, T, Tdg, U1, CU, CNOT, CCX, SWAP
+export id, X, Y, Z, H, S, Sdg, T, Tdg, U1, U2, U3, CU, CNOT, CCX, SWAP
