49091d8f2b849cc42cc489aec2f5df547698818d
goropikari <goropikari56@gmail.com>
2018-04-18 21:24:47 +0900

    add help. add swap and singleunitary

--- src/gates.jl
+++ src/gates.jl
@@ -1,4 +1,5 @@
-export hadamard, sgate, sdggate, tgate, tdggate, phaseshift, controlsgate, cnot, toffoli
+export hadamard, sgate, sdggate, tgate, tdggate, phaseshift, sigleunitary,
+       controlsgate, cnot, toffoli, swap
 
 # quantum circuit gates
 sigmam() = sigmam(SpinBasis(1//2))
@@ -7,6 +8,7 @@ sigmax() = sigmax(SpinBasis(1//2))
 sigmay() = sigmay(SpinBasis(1//2))
 sigmaz() = sigmaz(SpinBasis(1//2))
 hadamard(n::Int=1) = tensor([DenseOperator(SpinBasis(1//2), 1/sqrt(2)*[1 1; 1 -1]) for i in 1:n]...)
+
 """
        sgate()
 
@@ -76,8 +78,9 @@ function tdggate()
 end
 
 """
-       phaseshift(λ)
+       phaseshift(λ::Real)
 
+phase shift gate
 ```math
 U(\\lambda) = \\begin{bmatrix}
        1 & 0 \\\\
@@ -92,8 +95,36 @@ function phaseshift(λ::Real)
        return SparseOperator(SpinBasis(1//2), x)
 end
 
+
+function sigleunitary(θ, ϕ, λ)
+       x = spzeros(Complex{Float64}, 2, 2)
+       x[1,1] = cos(θ/2)
+       x[1,2] = - exp(im * λ) * sin(θ/2)
+       x[2,1] = exp(im * ϕ) * sin(θ/2)
+       x[2,2] = exp(im * (λ+ϕ)) * cos(θ/2)
+       return SparseOperator(SpinBasis(1//2), x)
+end
+
 """
-       controlsgate(N, controls, target, op)
+       controlsgate(N::Int, controls::Vector{Int}, target::Int, op::Operator)
+
+multi-controlled gate.
+# Example
+```julia
+julia> nqubits, controls, target, op = 3, [1, 2], 3, sigmax();
+
+julia> controlsgate(nqubits, controls, target, op) # equivalent to toffoli gate
+SparseOperator(dim=8x8)
+  basis: [Spin(1/2) ⊗ Spin(1/2) ⊗ Spin(1/2)]
+  [1, 1]  =  1.0+0.0im
+  [2, 2]  =  1.0+0.0im
+  [3, 3]  =  1.0+0.0im
+  [4, 4]  =  1.0+0.0im
+  [5, 5]  =  1.0+0.0im
+  [6, 6]  =  1.0+0.0im
+  [8, 7]  =  1.0+0.0im
+  [7, 8]  =  1.0+0.0im
+```
 """
 function controlsgate(N::Int, controls::Vector{Int}, target::Int, op::QuantumOptics.operators.Operator)
        nc = length(controls) # number of control qubits
@@ -131,16 +162,138 @@ function controlsgate(N::Int, controls::Vector{Int}, target::Int, op::QuantumOpt
 end
 
 """
-       cnot(N, control, target)
+       cnot(N::Int=2, control::Int=1, target::Int=2)
+
+# Example
+```julia
+julia> cnot()
+SparseOperator(dim=4x4)
+  basis: [Spin(1/2) ⊗ Spin(1/2)]
+  [1, 1]  =  1.0+0.0im
+  [2, 2]  =  1.0+0.0im
+  [4, 3]  =  1.0+0.0im
+  [3, 4]  =  1.0+0.0im
+
+julia> nqubits, control, target = 3, 1, 3;
+
+julia> cnot(nqubits, control, target)
+SparseOperator(dim=8x8)
+  basis: [Spin(1/2) ⊗ Spin(1/2) ⊗ Spin(1/2)]
+  [1, 1]  =  1.0+0.0im
+  [2, 2]  =  1.0+0.0im
+  [3, 3]  =  1.0+0.0im
+  [4, 4]  =  1.0+0.0im
+  [6, 5]  =  1.0+0.0im
+  [5, 6]  =  1.0+0.0im
+  [8, 7]  =  1.0+0.0im
+  [7, 8]  =  1.0+0.0im
+```
 """
-cnot(N::Int=2, control=1, target=2) = controlsgate(N, [control], target, sigmax())
+cnot(N::Int=2, control::Int=1, target::Int=2) = controlsgate(N, [control], target, sigmax())
 
 """
-       toffoli(N, controls, target)
+       toffoli(N::Int=3, controls::Vector{Int}=[1, 2], target::Int=3)
+
+# Example
+```julia
+julia> toffoli() # equivalent to  toffoli(3, [1,2], 3)
+SparseOperator(dim=8x8)
+  basis: [Spin(1/2) ⊗ Spin(1/2) ⊗ Spin(1/2)]
+  [1, 1]  =  1.0+0.0im
+  [2, 2]  =  1.0+0.0im
+  [3, 3]  =  1.0+0.0im
+  [4, 4]  =  1.0+0.0im
+  [5, 5]  =  1.0+0.0im
+  [6, 6]  =  1.0+0.0im
+  [8, 7]  =  1.0+0.0im
+  [7, 8]  =  1.0+0.0im
+
+julia> nqubits, controls, target = 4, [1,2], 3;
+
+julia> toffoli(nqubits, controls, target)
+SparseOperator(dim=16x16)
+  basis: [Spin(1/2) ⊗ Spin(1/2) ⊗ Spin(1/2) ⊗ Spin(1/2)]
+  [1 ,  1]  =  1.0+0.0im
+  [2 ,  2]  =  1.0+0.0im
+  [3 ,  3]  =  1.0+0.0im
+  [4 ,  4]  =  1.0+0.0im
+  [5 ,  5]  =  1.0+0.0im
+  [6 ,  6]  =  1.0+0.0im
+  [7 ,  7]  =  1.0+0.0im
+  [8 ,  8]  =  1.0+0.0im
+  [9 ,  9]  =  1.0+0.0im
+  [10, 10]  =  1.0+0.0im
+  [11, 11]  =  1.0+0.0im
+  [12, 12]  =  1.0+0.0im
+  [15, 13]  =  1.0+0.0im
+  [16, 14]  =  1.0+0.0im
+  [13, 15]  =  1.0+0.0im
+  [14, 16]  =  1.0+0.0im
+```
 """
 function toffoli(N::Int=3, controls::Vector{Int}=[1, 2], target::Int=3)
        if N < 3
               error("the number of qubits must be larger or equal to 3.")
        end
        return controlsgate(N, controls, target, sigmax())
+end
+
+"""
+       swap(N::Int=2, targets::Vector{Int}=[1,2])
+
+# Exapmle
+```julia
+julia> swap()
+SparseOperator(dim=4x4)
+  basis: [Spin(1/2) ⊗ Spin(1/2)]
+  [1, 1]  =  1.0+0.0im
+  [3, 2]  =  1.0+0.0im
+  [2, 3]  =  1.0+0.0im
+  [4, 4]  =  1.0+0.0im
+
+julia> swap(3, [1,2])
+SparseOperator(dim=8x8)
+  basis: [Spin(1/2) ⊗ Spin(1/2) ⊗ Spin(1/2)]
+  [1, 1]  =  1.0+0.0im
+  [2, 2]  =  1.0+0.0im
+  [5, 3]  =  1.0+0.0im
+  [6, 4]  =  1.0+0.0im
+  [3, 5]  =  1.0+0.0im
+  [4, 6]  =  1.0+0.0im
+  [7, 7]  =  1.0+0.0im
+  [8, 8]  =  1.0+0.0im
+```
+"""
+function swap(N::Int=2, targets::Vector{Int}=[1,2])
+       if N < 2
+              error("the number of qubits must be larger or equal to 2.")
+       end
+       if targets[1] == targets[2]
+              error("target qubits mustn't be same.")
+       end
+       if length(targets) != 2
+              error("the number of target qubits must be equal to 2")
+       end
+       x = spzeros(Complex{Float64}, 4, 4)
+       x[1,1] = 1.
+       x[3,2] = 1.
+       x[2,3] = 1.
+       x[4,4] = 1.
+       basicswap = SparseOperator(SpinBasis(1//2)^2, x)
+
+       control, target = targets[1], targets[2]
+       if N == 2
+              return basicswap
+       else
+              perm = collect(1:N)
+              targetpos = N
+              state = identityoperator(SpinBasis(1//2)^(N-2)) ⊗ basicswap
+              perm[control], perm[end-1] = perm[end-1], perm[control]
+              if control == N
+                     targetpos = N-1
+              end
+              perm[target], perm[targetpos] = perm[targetpos], perm[target]
+
+              return permutesystems(state, perm)
+       end
 end
